51b302c87cc91940df788b04613d30702eefbece
Tim <tim@example.com>
2021-08-29 10:57:28 -0500

    BC-86

--- EurekaServer/src/test/java/com/smoothstack/EurekaServer/EurekaServerApplicationTests.java
+++ EurekaServer/src/test/java/com/smoothstack/EurekaServer/EurekaServerApplicationTests.java
@@ -1,13 +1,47 @@
+
+
 package com.smoothstack.EurekaServer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-@SpringBootTest
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@LocalServerPort
+	int randomServerPort;
+
+	@Test
+	public void testDashboard() throws URISyntaxException {
+		final String baseUrl = "http://localhost:" + randomServerPort + "/";
+		URI uri = new URI(baseUrl);
+
+		HttpHeaders headers = new HttpHeaders();
+
+		ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers),
+				String.class);
+
+		Assert.assertEquals(HttpStatus.OK, result.getStatusCode());
+	}
+	
 }
+
