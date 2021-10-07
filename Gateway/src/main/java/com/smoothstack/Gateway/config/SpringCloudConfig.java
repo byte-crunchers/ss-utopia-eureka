febbc20e65220d51b59d2223400d5feb197d0626
tim-ning <87076278+tim-ning@users.noreply.github.com>
2021-10-07 01:37:54 -0500

    BC-77 updated routes for account microservice

--- Gateway/src/main/java/com/smoothstack/Gateway/config/SpringCloudConfig.java
+++ Gateway/src/main/java/com/smoothstack/Gateway/config/SpringCloudConfig.java
@@ -12,11 +12,13 @@ public class SpringCloudConfig {
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
 
 		return builder.routes().route(r -> r.path("/login").uri("lb://AUTH-CLIENT"))
-				.route(r -> r.path("/api/v0.1/CardTypes/**").uri("lb://CARD-CLIENT"))
+				.route(r -> r.path("/api/v0.1/accountTypes/**").uri("lb://ACCOUNT-CLIENT"))
+				.route(r -> r.path("/api/v0.1/accounts/**").uri("lb://ACCOUNT-CLIENT"))
 				.route(r -> r.path("/api/v0.1/LoanTypes/**").uri("lb://LOAN-CLIENT"))
-				.route(r -> r.path("/cards/**")
+//				.route(r -> r.path("/users/**").uri("lb://LOAN-CLIENT"))
+				.route(r -> r.path("/api/v0.1/cards/**")
 						.filters(f -> f.rewriteLocationResponseHeader("AS_IN_REQUEST", "Location", null, "https"))
-						.uri("lb://CARD-CLIENT"))
+						.uri("lb://ACCOUNT-CLIENT"))
 				.route(r -> r.path("/loans/**")
 						.filters(f -> f.rewriteLocationResponseHeader("AS_IN_REQUEST", "Location", null, "https"))
 						.uri("lb://LOAN-CLIENT"))
